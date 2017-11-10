bee86c3e9f65c440ee7c7d811cd2c2c1158ef6be
Mike Over <mike.over@teamsnap.com>
2017-11-10 07:46:10 -0500

    Use promise on signin to redirect user to /feature after

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -9,15 +10,18 @@ export function signinUser({ email, password }) {
   // multiple actions from a single action creator and handle side-effects.
   return function(dispatch) {
     // Submit email/password to the server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // If request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - redirect to the route '/feature'
-
-    // If request is bad...
-    // - Show an error to the user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        // - Save the JWT token
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+      });
   };
 
 }
